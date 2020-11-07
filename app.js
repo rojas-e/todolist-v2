79421f0c0666c2b53bbb20575b7894e6de51f9aa
Jonathan Rojas <jonrojas.dev@gmail.com>
2020-11-06 23:11:20 -0800

    add post route and ejs variables

--- app.js
+++ app.js
@@ -6,15 +6,17 @@ const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 
+let itemArray = [];
 
 app.get('/', function(req, res) {
-  res.render('index');
+  res.render('index', {item: itemArray});
 });
 
-
-
-
-
+app.post('/', function(req, res) {
+  let todoItem = req.body.todo;
+  itemArray.push(todoItem);
+  res.redirect('/');
+})
 
 
 app.listen(3000, function() {
